bc6049131336e2a3c1c70a79d86beee13d1906a9
cyq_hps <851953159@qq.com>
2019-07-10 14:41:55 +0800

    提交工程

---
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,15 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2019/7/9 18:05
+ * @Version V1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String address;
+}
